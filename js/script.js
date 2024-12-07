e78afe6e1def963eb0d4f8c29b447814b41aee03
Tevin Campbell <tevinc1991@icloud.com>
2024-12-06 21:53:31 -0500

    Enhance tool section with tool cards and filtering functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const buttons = document.querySelectorAll(".tool-btns");
+const toolCards = document.querySelectorAll(".tool-card");
 
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
@@ -8,5 +9,12 @@ buttons.forEach((button) => {
       btn.classList.remove("selected");
     });
     this.classList.add("selected");
+    if (this.classList.contains("language")) {
+      toolCards.forEach((card) => {
+        if (!card.classList.contains("language")) {
+          card.style.display = "none";
+        }
+      });
+    }
   });
 });
